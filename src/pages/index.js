9510c616e164be7411b367f0b1b1a4172dcf97e4
Prabhat <prabhatsingh55rocks@gmail.com>
2022-05-06 22:11:51 +0530

    restore index.js file in pages

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import {Home} from "./Home/Home"
+import {LikePage} from "./LikePage/LikePage"
+import {PlayList} from "./PlayList/PlayList"
+import {WatchLater} from "./WatchLater/WatchLater"
+import { Mock } from "./Mock/Mock"
+import { Explore } from "./Explore/Explore"
+import {Login} from "./Login/Login"
+
+export {Home,LikePage,PlayList,WatchLater,Mock,Explore,Login}  
