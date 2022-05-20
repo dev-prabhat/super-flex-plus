aee8663db2d93706fbf9973d7df720c3b2aea06c
Prabhat <prabhatsingh55rocks@gmail.com>
2022-05-20 16:43:57 +0530

    feat: done with signup page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,5 +7,6 @@ import { Explore } from "./Explore/Explore"
 import {Login} from "./Login/Login"
 import { SinglePlayer } from "./SinglePlayer/SinglePlayer"
 import { History } from "./History/History"
-
-export {Home,LikePage,PlayList,WatchLater,Mock,Explore,Login,SinglePlayer,History}  
+import { SignUp } from "./SignUp/SignUp"
+ 
+export {Home,LikePage,PlayList,WatchLater,Mock,Explore,Login,SinglePlayer,History,SignUp}  
